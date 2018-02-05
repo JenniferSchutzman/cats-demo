67071a69e74831d78330e5f00c19f7103f9398b4
Jennifer Schutzman <jennifer.schutzman@gmail.com>
2018-02-05 14:15:29 -0500

    added get

--- api.js
+++ api.js
@@ -9,3 +9,10 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => res.send('meow')  )
 
 app.listen(port, () => console.log("CATS!", port))
+
+const cats = [
+  {name: "Monster", age: 76, gender: "female", breed: "DontCare"},
+  {name: "Pat", age:, 5, gender: "trans", breed: "NoMeImporta"},
+]
+
+app.get ('/cats', (req, res) => res.send(cats))
